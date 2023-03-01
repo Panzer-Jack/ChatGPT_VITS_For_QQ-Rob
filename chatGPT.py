1b73d210484246b2d34338743a935f12ce74c01f
Panzer-Jack <81006731+Panzer-Jack@users.noreply.github.com>
2023-03-01 10:51:16 +0800

    Update chatGPT.py

--- chatGPT.py
+++ chatGPT.py
@@ -18,7 +18,7 @@ def chatGPT_ask(prompt, prev_text=prev_text):
         print(message, end="", flush=True)
         prev_text = data["message"]
     print("\n")
-    vits_voice(txt=prev_text, speaker=78, language=0)
+    vits_voice(txt=prev_text, speaker=78, language=config.language)
 
 
 print()
